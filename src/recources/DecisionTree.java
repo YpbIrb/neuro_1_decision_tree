27344b14e1a186d57c6277a3cb7084f2c97cc019
Ivan Smal <vanasmal@mail.ru>
2020-03-14 02:16:18 +0700

    .

--- src/recources/DecisionTree.java
+++ src/recources/DecisionTree.java
@@ -19,7 +19,7 @@ public class DecisionTree {
 
     public void MakeLeaf(TreeNode node, String attr_val, Map<String, Integer> freq){
         int max_freq = 0;
-        String new_label;
+        String new_label = new String();
         for (Map.Entry<String, Integer> entry: freq.entrySet()){
             if(max_freq < entry.getValue()){
                 max_freq = entry.getValue();
@@ -27,7 +27,7 @@ public class DecisionTree {
             }
         }
 
-        node.AddChild(attr_val, new TreeNode(new_label));
+        node.AddChild(attr_val, new TreeNode(null,new_label));
 
     }
 
--- src/recources/TreeNode.java
+++ src/recources/TreeNode.java
@@ -8,6 +8,12 @@ public class TreeNode {
     private String final_val;
     private Map<String, TreeNode> childrens;
 
+    public TreeNode(String attr_name, String final_val) {
+        this.attr_name = attr_name;
+        this.final_val = final_val;
+        childrens = new HashMap<>();
+    }
+
     public TreeNode() {
         this.attr_name = null;
         final_val = null;
